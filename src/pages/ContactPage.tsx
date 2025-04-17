9c50a6dcc6bcb870a1845dc2689c7216a7857d43
varmanli <varmanliamirhosein@gmail.com>
2025-04-17 22:55:12 +0330

    fix padding

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -3,13 +3,13 @@ import ButtonLink from "../components/ui/ButtonLink";
 
 export default function ContactPage() {
   return (
-    <section className="relative bg-black text-white py-24 md:py-32 px-4 sm:px-6 md:px-20 min-h-screen overflow-hidden">
+    <section className="relative bg-black text-white py-24 md:py-32 sm:px-6 md:px-20 min-h-screen overflow-hidden">
       {/* Gradient Light */}
       <div className="absolute top-0 left-0 w-full h-140 bg-gradient-to-b from-white/10 via-transparent to-transparent pointer-events-none z-0" />
 
-      <div className="relative z-10 max-w-6xl mx-auto space-y-24">
+      <div className="relative z-10 max-w-6xl mx-auto space-y-24 ">
         {/* Title */}
-        <div className="text-center max-w-3xl mx-auto" data-aos="fade-up">
+        <div className="text-center max-w-3xl mx-auto px-5" data-aos="fade-up">
           <h1 className="text-4xl md:text-5xl font-bold text-primary-gold font-serif mb-4">
             Get in Touch
           </h1>
